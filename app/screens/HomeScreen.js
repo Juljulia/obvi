fae39fd7ac7ac9172de1e60c89bb5e42d5ad3ac8
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-10-16 12:35:07 +0200

    fetch from google API

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -6,17 +6,19 @@ import {
   ActivityIndicator,
 } from "react-native";
 
-import useAuth from "../auth/useAuth";
 import Button from "../components/Button";
-import usersApi from "../api/users";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import getEnvVars from "../../environment";
 import mapStyle from "./../config/mapStyle";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import useAuth from "../auth/useAuth";
+import usersApi from "../api/users";
 
 function HomeScreen(props) {
   const { user, logOut } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
   const [userData, setUserData] = useState();
   const [locationList, setLocationList] = useState();
+  const { googlePlacesApiKey } = getEnvVars();
 
   const getUserData = async () => {
     const data = await usersApi.getUser(user.uid);
@@ -25,7 +27,9 @@ function HomeScreen(props) {
   };
 
   const getLocationList = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=${googlePlacesApiKey}`
+    );
     const json = await response.json();
     setLocationList(json);
   };
@@ -38,27 +42,29 @@ function HomeScreen(props) {
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <>
-          <Text>Welcome {userData["username"]}</Text>
-          <Button title="Logout" onPress={() => logOut()} />
-          <MapView
-            style={styles.map}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-              latitude: 57.70547,
-              longitude: 11.968415,
-              latitudeDelta: 0.015, //avgör hur inzoomat det ska vara från början
-              longitudeDelta: 0.0121,
-            }}
-            customMapStyle={mapStyle}
-          ></MapView>
-        </>
-      )}
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <>
+            <Text>Welcome {userData["username"]}</Text>
+            <Button title="Logout" onPress={() => logOut()} />
+          </>
+        )}
+      </SafeAreaView>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 57.70547,
+          longitude: 11.968415,
+          latitudeDelta: 0.015, //avgör hur inzoomat det ska vara från början
+          longitudeDelta: 0.0121,
+        }}
+        customMapStyle={mapStyle}
+      ></MapView>
+    </>
   );
 }
 
