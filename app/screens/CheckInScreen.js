97cbdb9b025c0eb6acc53d973a0712b7a5463919
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-10-19 12:49:57 +0200

    get places based on expo-location

--- app/screens/CheckInScreen.js
+++ app/screens/CheckInScreen.js
@@ -10,36 +10,35 @@ function CheckInScreen(props) {
   const location = useLocation();
   const [placesList, setPlacesList] = useState();
   const { googlePlacesApiKey } = getEnvVars();
+  const [showPlaces, setShowPlaces] = useState(false);
 
   const getPlacesList = async () => {
-    // FIX THE PROBLEM WITH FETCH WITH LOCATION, location is undefined or something
-    // if (location) {
-    //   const response = await fetch(
-    //     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=1500&key=${googlePlacesApiKey}`
-    //   );
-    //   const json = await response.json();
-    //   setPlacesList(json);
-    // }
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=nordic%20wellness&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=${googlePlacesApiKey}`
-    );
-    const json = await response.json();
-    setPlacesList(json);
+    if (location) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location["latitude"]},${location["longitude"]}&radius=1000&key=${googlePlacesApiKey}`
+      );
+      const json = await response.json();
+      setPlacesList(json.results);
+      setShowPlaces(true);
+    }
   };
+
+  useEffect(() => {
+    getPlacesList();
+  }, []);
 
   const searchPlaces = async (value) => {
     const response = await fetch(
       `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${value}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=${googlePlacesApiKey}`
     );
     const json = await response.json();
-    setPlacesList(json);
-    console.log(placesList);
+    setPlacesList(json.candidates);
+    setShowPlaces(true);
+    if (json.status === "INVALID_REQUEST") {
+      setShowPlaces(false);
+      getPlacesList();
+    }
   };
-
-  useEffect(() => {
-    getPlacesList();
-    console.log(location);
-  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -50,12 +49,12 @@ function CheckInScreen(props) {
         onChangeText={(value) => searchPlaces(value)}
       />
       <View>
-        {placesList ? (
-          placesList.candidates.map((location, key) => (
+        {!showPlaces ? (
+          <Text>No result</Text>
+        ) : (
+          placesList.map((location, key) => (
             <Text key={key}>{location.name}</Text>
           ))
-        ) : (
-          <Text>No result</Text>
         )}
       </View>
     </SafeAreaView>
