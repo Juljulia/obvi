93da71f21c97d79ae566bbf62375a5e4e0baff0e
emeliepetersson <emepet1204@skola.goteborg.se>
2020-10-19 16:49:35 +0200

    make it possible to check in

--- app/screens/CheckInScreen.js
+++ app/screens/CheckInScreen.js
@@ -1,48 +1,99 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  View,
+  Alert,
+} from "react-native";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
+import colors from "../config/colors";
 import getEnvVars from "../../environment";
+import ListItemSeparator from "../components/lists/ListItemSeparator";
+import navigation from "../navigation/rootNavigation";
+import routes from "../navigation/routes";
+import Screen from "../components/Screen";
 import Text from "../components/Text";
 import TextInput from "../components/TextInput";
+import useAuth from "../auth/useAuth";
 import useLocation from "../hooks/useLocation";
 
-function CheckInScreen(props) {
+function CheckInScreen() {
+  const { user } = useAuth();
   const location = useLocation();
-  const [placesList, setPlacesList] = useState();
+  const [places, setPlaces] = useState();
   const { googlePlacesApiKey } = getEnvVars();
   const [showPlaces, setShowPlaces] = useState(false);
+  const db = firebase.firestore();
 
-  const getPlacesList = async () => {
+  const getPlaces = async () => {
     if (location) {
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location["latitude"]},${location["longitude"]}&radius=1000&key=${googlePlacesApiKey}`
       );
       const json = await response.json();
-      setPlacesList(json.results);
+      setPlaces(json.results);
       setShowPlaces(true);
     }
   };
 
   useEffect(() => {
-    getPlacesList();
-  }, []);
+    getPlaces();
+  }, [location]);
 
   const searchPlaces = async (value) => {
     const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${value}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=${googlePlacesApiKey}`
+      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${value}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry,types,place_id&key=${googlePlacesApiKey}`
     );
     const json = await response.json();
-    setPlacesList(json.candidates);
+    setPlaces(json.candidates);
     setShowPlaces(true);
     if (json.status === "INVALID_REQUEST") {
       setShowPlaces(false);
-      getPlacesList();
+      getPlaces();
     }
   };
 
+  const handlePress = (item) => {
+    Alert.alert("Check-in", "Are you sure you want to check-in?", [
+      {
+        text: "No",
+        style: "cancel",
+      },
+      { text: "Yes", onPress: () => storeCheckIn(item) },
+    ]);
+  };
+
+  const storeCheckIn = (item) => {
+    console.log(item);
+    const place = {
+      name: item.name,
+      geometry: item.geometry,
+      // adress: item.formatted_address,
+      placeId: item.place_id,
+      categories: item.types,
+      userId: user.uid,
+    };
+
+    db.collection("checkIns")
+      .doc(user.uid)
+      .collection("userCheckIns")
+      .doc()
+      .set(place)
+      .then(function () {
+        console.log("Document successfully written!");
+      })
+      .catch(function (error) {
+        console.error("Error writing document: ", error);
+      });
+    navigation.navigate(routes.HOME);
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Check In</Text>
+    <Screen>
+      <Text>Check-In</Text>
       <TextInput
         placeholder="Where are you?"
         icon="map-search-outline"
@@ -52,17 +103,29 @@ function CheckInScreen(props) {
         {!showPlaces ? (
           <Text>No result</Text>
         ) : (
-          placesList.map((location, key) => (
-            <Text key={key}>{location.name}</Text>
-          ))
+          <FlatList
+            data={places}
+            keyExtractor={(place) => place.place_id.toString()}
+            ItemSeparatorComponent={ListItemSeparator}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.places}
+                onPress={() => handlePress(item)}
+              >
+                <Text>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
         )}
       </View>
-    </SafeAreaView>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  places: {
+    padding: 10,
+  },
 });
 
 export default CheckInScreen;
