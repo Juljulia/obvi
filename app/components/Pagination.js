6eb3209e32817dddb8b7199aabf61706154cd4b1
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-10-22 17:08:38 +0200

    add multi screen form

---
+++ app/components/Pagination.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import Text from "./Text";
+
+function Pagination({ page, totalPages }) {
+  return (
+    <View style={styles.container}>
+      <Text>
+        {page} / {totalPages}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Pagination;
