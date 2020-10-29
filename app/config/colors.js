c9c8ad2273a37d982d8cc884d706f1eadf471d05
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-10-29 19:41:39 +0100

    change colors

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,10 +1,12 @@
 export default {
-  primary: "#fc5c65",
-  secondary: "#4ECDC4",
+  primary: "#4070F4",
+  medium: "#456078",
+  light: "#F2F3F7",
+  dark: "#0c0c0c", //text
   black: "#000",
   white: "#fff",
-  medium: "#6e6969",
-  light: "#f8f4f4",
+
+  //not used right now
+  secondary: "#4ECDC4",
   danger: "#ff5252",
-  dark: "#0c0c0c",
 };
