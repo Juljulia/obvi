d69f61a97dfaeb3a60e5311f033f8e678647c9d7
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-10-30 12:27:24 +0100

    update check-in

---
+++ app/components/Slider.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MultiSlider from "@ptomasroos/react-native-multi-slider";
+
+import CustomSliderMarker from "../components/CustomSliderMarker";
+import Text from "./Text";
+import colors from "../config/colors";
+
+function Slider({
+  onValuesChange,
+  values,
+  min = 1,
+  max = 13,
+  length = 315,
+  minLabel = "0h",
+  maxLabel = "12h",
+}) {
+  return (
+    <View>
+      <MultiSlider
+        containerStyle={{
+          height: 96,
+          justifyContent: "flex-end",
+          marginLeft: 30,
+        }}
+        trackStyle={{ height: 15, borderRadius: 36 }}
+        selectedStyle={{
+          backgroundColor: colors.light,
+          backgroundColor: "transparent",
+          borderColor: "#F0F0F3",
+          borderWidth: 1,
+          overflow: "hidden",
+          shadowColor: "#A0A5B9",
+          shadowRadius: 9,
+          shadowOpacity: 1,
+        }}
+        onValuesChange={onValuesChange}
+        min={min}
+        max={max}
+        sliderLength={length}
+        customMarker={(e) => {
+          return <CustomSliderMarker text={e.currentValue} />;
+        }}
+        markerOffsetY={-20}
+        values={values}
+      />
+      <View style={styles.timeRange}>
+        <Text>{minLabel}</Text>
+        <Text>{maxLabel}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  timeRange: {
+    marginLeft: 30,
+    width: 315,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+});
+
+export default Slider;
--- app/components/forms/SubmitButton.js
+++ app/components/forms/SubmitButton.js
@@ -3,10 +3,17 @@ import { useFormikContext } from "formik";
 
 import Button from "../Button";
 
-function SubmitButton({ title }) {
+function SubmitButton({ title, style, disabledStyle, ...otherProps }) {
   const { handleSubmit } = useFormikContext();
 
-  return <Button title={title} onPress={handleSubmit} />;
+  return (
+    <Button
+      title={title}
+      onPress={handleSubmit}
+      {...otherProps}
+      disabledStyle={disabledStyle}
+    />
+  );
 }
 
 export default SubmitButton;
