232cbc7c24980044f96a91c33d87699ca6b6d304
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-10-30 14:37:57 +0100

    add content home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, View } from "react-native";
 
 import Button from "../components/Button";
+import Card from "../components/Card";
 import mapStyle from "./../config/mapStyle";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import routes from "../navigation/routes";
+import Text from "../components/Text";
 import Screen from "../components/Screen";
 import useAuth from "../auth/useAuth";
 import usersApi from "../api/users";
 import useLocation from "../hooks/useLocation";
+import { ScrollView } from "react-native-gesture-handler";
+import colors from "../config/colors";
 
 function HomeScreen({ navigation }) {
   const { user, logOut } = useAuth();
@@ -40,34 +44,56 @@ function HomeScreen({ navigation }) {
   }, [location]);
 
   return (
-    <Screen style={styles.container}>
-      {isLoading && !userData ? (
-        <ActivityIndicator />
-      ) : (
-        <View style={styles.welcome}>
-          <Text>Hello {userData["username"]}</Text>
-          <Button title="Logout" onPress={() => logOut()} />
-        </View>
-      )}
-      {region && (
-        <MapView
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          region={region}
-          customMapStyle={mapStyle}
-          showsMyLocationButton
-          showsUserLocation
-          region={region}
-          onPress={() => navigation.navigate(routes.MAP)}
-        ></MapView>
-      )}
-    </Screen>
+    <ScrollView>
+      <Screen style={styles.container}>
+        {isLoading && !userData ? (
+          <ActivityIndicator />
+        ) : (
+          <View style={styles.welcome}>
+            <Text>Hello {userData["username"]}</Text>
+            <Button title="Logout" onPress={() => logOut()} />
+          </View>
+        )}
+        <Card
+          title="The Obvi map"
+          // image={require("../assets/union.png")}
+          subTitle="Show friends within the community"
+        >
+          {region && (
+            <MapView
+              style={styles.map}
+              provider={PROVIDER_GOOGLE}
+              region={region}
+              customMapStyle={mapStyle}
+              showsUserLocation
+              region={region}
+              onPress={() => navigation.navigate(routes.MAP)}
+            ></MapView>
+          )}
+          <Image
+            source={require("../assets/wave.png")}
+            style={styles.wave}
+          ></Image>
+        </Card>
+        <Card
+          title="Check in"
+          image={require("../assets/union.png")}
+          subTitle="Show friends your exact location"
+        ></Card>
+        <Card
+          title="Calendar"
+          image={require("../assets/union.png")}
+          subTitle="See upcoming events"
+        ></Card>
+      </Screen>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
   },
   map: {
     flex: 1,
@@ -76,6 +102,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     height: "30%",
+  },
+  wave: {
+    position: "absolute",
+    bottom: -28,
+    left: -24.5,
   },
 });
 
