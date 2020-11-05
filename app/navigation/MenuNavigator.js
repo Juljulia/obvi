2087f2efd7285ae9003689d2bd4f6bfc5e278075
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-11-05 13:43:12 +0100

    add navigator to homescreen and clean up code

--- app/navigation/MenuNavigator.js
+++ app/navigation/MenuNavigator.js
@@ -2,9 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import ModalMenu from "../components/nav/ModalMenu";
-import HomeScreen from "../screens/HomeScreen";
-import MapScreen from "../screens/MapScreen";
-import CheckInScreen from "../screens/CheckInScreen";
 import AccountScreen from "../screens/AccountScreen";
 
 const Stack = createStackNavigator();
@@ -14,21 +11,6 @@ const MenuNavigator = () => (
     <Stack.Screen
       name="Menu"
       component={ModalMenu}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Map"
-      component={MapScreen}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Check-in"
-      component={CheckInScreen}
       options={{ headerShown: false }}
     />
     <Stack.Screen
