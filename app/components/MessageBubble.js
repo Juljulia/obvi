3fe0ebf967d15ea7125dd41832f2e580947b645a
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-11-06 16:46:25 +0100

    change maxLength of message input field and update its styling

--- app/components/MessageBubble.js
+++ app/components/MessageBubble.js
@@ -3,24 +3,33 @@ import { StyleSheet, Image, View } from "react-native";
 
 import Text from "../components/typography/Text";
 
-function MessageBubble({ text, style }) {
+function MessageBubble({ text, style, width = 380 }) {
   return (
-    <View style={style}>
-      <Image source={require("../assets/shout-out.png")} />
+    <View style={[styles.container, { width, height: 127 }, style]}>
+      <Image
+        style={{ width }}
+        source={require("../assets/shout-out.png")}
+        resizeMode="stretch"
+      />
       <Text style={styles.text}>{text}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
   text: {
     shadowOffset: { width: 3.74, height: 3.74 },
     shadowColor: "#BECDE2",
     shadowRadius: 8,
     shadowOpacity: 1,
     position: "absolute",
-    top: 60,
-    left: 55,
+    top: 20,
+    left: 50,
+    width: "80%",
   },
 });
 
