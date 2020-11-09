d458923bad6dd7da839990433eda417e563f95c1
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-11-09 17:25:22 +0100

    clean up

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: colors.light,
+    backgroundColor: colors.basicGrey,
   },
   view: {
     flex: 1,
