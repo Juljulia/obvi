a6024f4e15f65f19416d7990584a4229812ae9da
emeliepetersson <emepet1204@skola.goteborg.se>
2020-11-09 12:06:31 +0100

    update styling on NavArrow and add enable/disable location in CheckInScreen

--- app/components/nav/NavArrow.js
+++ app/components/nav/NavArrow.js
@@ -29,11 +29,11 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
-    shadowOffset: { width: 7, height: 7 },
+    shadowOffset: { width: 3.74, height: 3.74 },
     shadowColor: colors.shadow,
-    shadowRadius: 10,
+    shadowRadius: 4.5,
     shadowOpacity: 1,
-    backgroundColor: colors.white,
+    backgroundColor: colors.basicGrey,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 25,
@@ -41,6 +41,8 @@ const styles = StyleSheet.create({
   },
   arrow: (goBack) => ({
     transform: goBack ? [{ rotate: "180deg" }] : [],
+    width: 8,
+    height: 12,
   }),
 });
 
